205fc21a03aad8c21cc36072d68002261958b043
Daniel Alejandre <65686123+Juanda1050@users.noreply.github.com>
2022-07-06 14:43:10 -0500

    Agregando funciones para cada accion

--- Sketch-2/Fisico.cpp
+++ Sketch-2/Fisico.cpp
@@ -6,6 +6,7 @@
 #define Type DHT11
 
 int dhtPin = 10;
+int alarm = 6;
 DHT HT(dhtPin, Type);
 SFE_BMP180 sensorPressure;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
@@ -18,6 +19,7 @@ double seaLevel = 1014.0;
 void setup()
 {
   Serial.begin(9600);
+  pinMode(alarm, OUTPUT);
   HT.begin();
   sensorPressure.begin();
   lcd.begin(16, 2);
@@ -27,37 +29,7 @@ void setup()
 void loop()
 {
   
-  humidity = HT.readHumidity();
-  tempC = HT.readTemperature();
-  
-  status = sensorPressure.startTemperature();
-  delay(status);
-  sensorPressure.getTemperature(tempPressure);
-  status = sensorPressure.startPressure(3);
-  delay(status);
-  sensorPressure.getPressure(pressure, tempPressure);
-  relativePressure = pressure * 0.1;
-  altitudeSea = sensorPressure.altitude(pressure, seaLevel);
-  
-  Serial.println(humidity);
-  Serial.println(tempC);
-  String temp_output = "TEMP: " + String(tempC) + String((char)223) + "C";
-  String hum_output = "HUMEDAD: " + String(humidity) + "%";
-  String pres_output = "PRES: " + String(relativePressure) + " KPa";
-  String alt_output = "P.ATM: " + String(altitudeSea) + " m";
-  
-  lcd.setCursor(0, 0);
-  lcd.print(temp_output);
-  lcd.setCursor(0, 1);
-  lcd.print(hum_output);
-  delay(3000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(pres_output);
-  lcd.setCursor(0, 1);
-  lcd.print(alt_output);
-  delay(3000);
-  lcd.clear();
+  setValues();
   
 }
 
@@ -73,4 +45,54 @@ void startupMessage() {
   delay(2000);
   lcd.clear();
   delay(100);
+}
+
+void setValues(){
+  humidity = HT.readHumidity();
+  tempC = HT.readTemperature();
+  
+  status = sensorPressure.startTemperature();
+  delay(status);
+  sensorPressure.getTemperature(tempPressure);
+  status = sensorPressure.startPressure(3);
+  delay(status);
+  sensorPressure.getPressure(pressure, tempPressure);
+  relativePressure = pressure * 0.1;
+  altitudeSea = sensorPressure.altitude(pressure, seaLevel);
+  
+  Serial.println(humidity);
+  Serial.println(tempC);
+
+   displayValues(humidity, tempC, relativePressure, altitudeSea);
+}
+
+void displayValues(float humidity, float tempC, float relativePressure, float altitudeSea){
+  String temp_output = "TEMP: " + String(tempC) + String((char)223) + "C";
+  String hum_output = "HUMEDAD: " + String(humidity) + "%";
+  String pres_output = "PRES: " + String(relativePressure) + " KPa";
+  String alt_output = "ALT: " + String(altitudeSea) + " m";
+  
+  lcd.setCursor(0, 0);
+  lcd.print(temp_output);
+  lcd.setCursor(0, 1);
+  lcd.print(hum_output);
+  tempAlarm(tempC);
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(pres_output);
+  lcd.setCursor(0, 1);
+  lcd.print(alt_output);
+  tempAlarm(tempC);
+  delay(3000);
+  lcd.clear();
+}
+
+void tempAlarm(float tempC){
+  if(tempC >= 36 || tempC < 5){
+    digitalWrite(alarm, HIGH);
+    delay(300);
+    digitalWrite(alarm, LOW);
+    delay(300);
+  }
 }

ae6a1e83451b36d7e26a7df56b894c206c1b9e93
Daniel Alejandre <65686123+Juanda1050@users.noreply.github.com>
2022-07-10 00:32:56 -0500

    Funciona cambiar entre pantallas

--- Sketch-2/Fisico.cpp
+++ Sketch-2/Fisico.cpp
@@ -18,6 +18,13 @@ double seaLevel = 1014.0;
 double auxPressure, auxHumidity, precipitation;
 
 const int nextBtn = 13;
+int displayValue = 0;
+
+void displayTemp(void);
+void displayPres(void);
+void displayPrecip(void);
+void setTemp(void);
+void showError(void);
 
 void setup()
 {
@@ -32,8 +39,32 @@ void setup()
 
 void loop()
 {
-  setValues();
-  lap++;  
+  if(digitalRead(nextBtn) == HIGH){
+    displayValue++;
+  }
+  switch(displayValue){
+    case 0:
+      lcd.clear();
+      displayTemp();
+    break;
+    case 1:
+      lcd.clear();
+      displayPres();
+    break;
+    case 2:
+      lcd.clear();
+      displayPrecip();
+    break;
+    case 3:
+      lcd.clear();
+      setTemp();
+    break;
+    default:
+      lcd.clear();
+      displayValue = 0;
+      displayTemp();
+  }
+  delay(1000);
 }
 
 void startupMessage() {
@@ -50,11 +81,13 @@ void startupMessage() {
   delay(100);
 }
 
-void setValues(){
-  auxPressure = relativePressure;
+void getDHT11(void){
   humidity = HT.readHumidity();
   tempC = HT.readTemperature();
-  
+}
+
+void getBMP180(void){
+  auxPressure = relativePressure;
   status = sensorPressure.startTemperature();
   delay(status);
   sensorPressure.getTemperature(tempPressure);
@@ -63,11 +96,6 @@ void setValues(){
   sensorPressure.getPressure(pressure, tempPressure);
   relativePressure = pressure * 0.750062;
   altitudeSea = sensorPressure.altitude(pressure, seaLevel);
-  
-  Serial.println("Humedad " + String(humidity));
-  Serial.println("Temperatura " + String(tempC));
-  Serial.println("Presion " + String(relativePressure));
-  Serial.println(counter);
 
   if(lap == 1){
     precipitation = 0;
@@ -81,37 +109,56 @@ void setValues(){
   else{
     precipitation = (relativePressure - auxPressure) / (humidity * 0.1) * 100;
   }
-  
-  Serial.println("Aux Humedad " + String(auxHumidity));
-  Serial.println("Aux Presion " + String(auxPressure));
-  Serial.println("Precipitacion " + String(precipitation));
-  Serial.println("");
 
-  displayValues(humidity, tempC, relativePressure, altitudeSea, precipitation);
+  lap++;
 }
 
-void displayValues(double humidity, double tempC, double relativePressure, double altitudeSea, double precipitation){ 
-  String temp_output = "T:" + String(tempC, 1) + String((char)223) + "C ";
-  String hum_output = "H:" + String(humidity, 0) + "%";
-  String pres_output = "BAR:" + String(relativePressure, 1) + " mmHg";
-  String alt_output = "ALT:" + String(altitudeSea) + " m";
-  String prec_output = "PRECIP:" + String(precipitation) + "%";
+void displayTemp(void){ 
+  getDHT11();
+  
+  String temp_output = "TEMP: " + String(tempC, 1) + String((char)223) + "C ";
+  String hum_output = "HUM: " + String(humidity, 0) + "%";
   
   lcd.setCursor(0, 0);
   lcd.print(temp_output);
-  lcd.print(hum_output);
   lcd.setCursor(0,1);
-  lcd.print(prec_output);
+  lcd.print(hum_output);
   tempAlarm(tempC);
-  delay(3000);
-  lcd.clear();
+}
+
+void displayPres(void){
+  getBMP180();
+  
+  String pres_output = "BAR: " + String(relativePressure, 1) + " mmHg";
+  String alt_output = "ALT: " + String(altitudeSea) + " m";
+  
   lcd.setCursor(0,0);
   lcd.print(pres_output);
   lcd.setCursor(0, 1);
   lcd.print(alt_output);
   tempAlarm(tempC);
-  delay(3000);
-  lcd.clear();
+}
+
+void displayPrecip(void){
+  getBMP180();  
+  String prec_output = "PRECIP: " + String(precipitation) + "%";
+  
+  lcd.setCursor(0,1);
+  lcd.print(prec_output);
+}
+
+void setTemp(){
+  String tempMin_output = "MIN: 5" + String((char)223) + "C";
+  String tempMax_output = "MAX: 36" + String((char)223) + "C";
+  lcd.setCursor(0, 0);
+  lcd.print(tempMin_output);
+  lcd.setCursor(0, 1);
+  lcd.print(tempMax_output);
+}
+
+void showError(){
+  lcd.setCursor(0, 0);
+  lcd.print("Error display");
 }
 
 void tempAlarm(float tempC){
